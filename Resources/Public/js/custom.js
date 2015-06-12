472d84e6e15814ec246342c891bf153fb30ac7e7
Norman Paschke <norman.paschke@dmk-ebusiness.de>
2015-06-12 12:14:48 +0200

    mobile Anpassungen

--- Resources/Public/js/custom.js
+++ Resources/Public/js/custom.js
@@ -1,8 +1,23 @@
 jQuery.noConflict();
 jQuery(document).ready(function($) {
+	
 	$('.nav-news').flexslider({
 		controlNav: false,
 		prevText: "vorheriges Element",
 		nextText: "nächstes Element"
 	});
+	
+	//Toggle Main Navigation (mobile only)
+    $('body').on('click','.nav-toggle',function() {
+        $('.nav-toggle').toggleClass('active');
+        $('.nav-mobile').toggleClass('open');
+    });
+	//tabs on mobile nav
+    $('body').on('click','.nav-mobile .tab > a',function(e) {
+		e.preventDefault();
+		$('.nav-mobile .tab').removeClass('active');
+		$('.nav-mobile .tab > ul').hide();
+        $(this).parent('.tab').addClass('active');
+		$(this).next('ul').show();
+    });
 });
