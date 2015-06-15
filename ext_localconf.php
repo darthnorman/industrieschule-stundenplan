da474605ebfe5eda0ff0d08fe1464b6f69c6b654
Sebastian Fritze <fritze.sebastian@web.de>
2015-06-15 08:40:20 +0200

    plugin wird wieder angezeigt

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,6 @@ if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
 Tx_Extbase_Utility_Extension::configurePlugin(
     $_EXTKEY,
-    'plan',
+    'Plan',
     array('Stundenplan' => 'list')
 );
--- ext_tables.php
+++ ext_tables.php
@@ -1,19 +1,8 @@
 <?php
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
-$TCA['tx_stundenplan_domain_model_product'] = array (
-    'ctrl' => array (
-        'title' => 'Stundenplan',
-        'label' => 'name',
-    ),
-    'columns' => array(
-    ),
-    'types' => array(
-    )
-);
-
 Tx_Extbase_Utility_Extension::registerPlugin(
     $_EXTKEY,
-    'List',
+    'Plan',
     'Stundenplan'
 );
