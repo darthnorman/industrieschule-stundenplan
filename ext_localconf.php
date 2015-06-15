c08115fc6ad6c388248f981010272ad7be05b7db
Sebastian Fritze <fritze.sebastian@web.de>
2015-06-15 11:24:02 +0200

    zeiten im stundenplan, caching deaktiviert

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,5 +4,6 @@ if (!defined ('TYPO3_MODE')) die ('Access denied.');
 Tx_Extbase_Utility_Extension::configurePlugin(
     $_EXTKEY,
     'Plan',
+    array('Stundenplan' => 'list'),
     array('Stundenplan' => 'list')
 );
